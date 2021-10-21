d015640331ebfa340c62c9d04d37ff6ac0b4d7c6
syykz <soyokazesutahamu@gmail.com>
2021-10-21 15:08:41 +0900

    nginxとのソケット共有設定を追記

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
