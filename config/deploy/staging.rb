4891b3c952f2a18dd2c48bad85e18b752d33830f
syykz <soyokazesutahamu@gmail.com>
2021-10-25 15:55:34 +0900

    cap: デプロイ先サーバの指定を修正(ロードバランサ対応)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server "www.syykz.net",
+server "ec2-user@ec2-13-114-54-253.ap-northeast-1.compute.amazonaws.com",
        user: "ec2-user",
        ssh_options: {
          keys: %w(~/.ssh/syykz_web.pem),
