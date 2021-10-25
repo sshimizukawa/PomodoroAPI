e7bac1a374abf94e28bf5425d67edce2957f3528
syykz <soyokazesutahamu@gmail.com>
2021-10-25 18:25:41 +0900

    prodution環境のログ設定を変更 * cap: staging設定の廃止 * prodution環境のlog_levelを変更 :info -> :error

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
-server "ec2-user@ec2-13-114-54-253.ap-northeast-1.compute.amazonaws.com",
-       user: "ec2-user",
-       ssh_options: {
-         keys: %w(~/.ssh/syykz_web.pem),
-       }
+# staging環境廃止
+# server "ec2-user@ec2-13-114-54-253.ap-northeast-1.compute.amazonaws.com",
+#        user: "ec2-user",
+#        ssh_options: {
+#          keys: %w(~/.ssh/syykz_web.pem),
+#        }
